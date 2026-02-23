9afd623f78042eb74ac197c7b6ec50be8d90c4a6
Eric Lippmann <eric.lippmann@icinga.com>
2026-02-23 21:48:08 +0100

    License source files as GPL-3.0-or-later
    Add SPDX license headers and mark source files as GPL-3.0-or-later to
    preserve the option to relicense under later GPL versions.

--- run.php
+++ run.php
@@ -1,4 +1,7 @@
 <?php
 
+// SPDX-FileCopyrightText: 2018 Icinga GmbH <https://icinga.com>
+// SPDX-License-Identifier: GPL-3.0-or-later
+
 $this->provideHook('director/ShipConfigFiles');
 $this->provideHook('director/ImportSource');
